69b94d67f7066051748b6acbea3be9304b28bf00
wherewhere <qq2518766683@outlook.com>
2023-12-10 00:59:39 +0800

    更新 Nuget

--- MicaForUWP/Media/BackgroundSource.cs
+++ MicaForUWP/Media/BackgroundSource.cs
@@ -1,4 +1,7 @@
-﻿namespace MicaForUWP.Media
+﻿using Windows.Foundation.Metadata;
+using Windows.Foundation;
+
+namespace MicaForUWP.Media
 {
     /// <summary>
     /// Defines values that specify whether the brush samples from the app content or from the content behind the app window.
@@ -8,16 +11,19 @@
         /// <summary>
         /// The brush samples from the content behind the app window.
         /// </summary>
+        [ContractVersion(typeof(UniversalApiContract), 262144u)]
         HostBackdrop = 0,
 
         /// <summary>
         /// The brush samples from the app content.
         /// </summary>
+        [ContractVersion(typeof(UniversalApiContract), 196608u)]
         Backdrop = 1,
 
         /// <summary>
         /// The brush samples from the wallpaper behind the app window.
-        /// </summary>  
+        /// </summary>
+        [ContractVersion(typeof(UniversalApiContract), 851968u)]
         WallpaperBackdrop = 2,
     }
 }
